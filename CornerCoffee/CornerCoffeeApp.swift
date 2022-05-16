31a335ab420f6b4ad4c77d3060dd46df2a782acf
Lucas Chae <hi@lucaschae.com>
2022-05-16 16:01:55 +0900

    Initial Commit

---
+++ CornerCoffee/CornerCoffeeApp.swift
@@ -0,0 +1,17 @@
+//
+//  CornerCoffeeApp.swift
+//  CornerCoffee
+//
+//  Created by Lucas Chae on 5/16/22.
+//
+
+import SwiftUI
+
+@main
+struct CornerCoffeeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
